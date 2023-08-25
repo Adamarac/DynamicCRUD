51e80bfe15b6073ccdca6d6827578cdb4cf86f13
Alan Moura <alan.moura007@gmail.com>
2023-08-24 23:06:47 -0300

    Table metadata
    Study on the implementation of search methods for information on the structure of each table.

--- src/main/java/com/CRUD/util/DataBaseConnection.java
+++ src/main/java/com/CRUD/util/DataBaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/teste";
+    private static final String URL = "jdbc:mysql://localhost:3306/db_cinema";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     
--- src/main/java/com/CRUD/util/main.java
+++ src/main/java/com/CRUD/util/main.java
@@ -1,20 +1,48 @@
 package com.CRUD.util;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class main {
 
     public static void main(String[] args) {
+                        
         try {
             Connection connection = DataBaseConnection.Connect();
 
             if (connection != null) {
                 System.out.println("Conexão bem-sucedida com o banco de dados!");
-                connection.close();
             } else {
                 System.out.println("Falha ao conectar ao banco de dados.");
             }
+            
+            DatabaseMetaData metaData = connection.getMetaData();
+            
+            String ler = JOptionPane.showInputDialog("Digite a tabela que deseja consultar");
+
+            String tableName = ler;
+            ResultSet columns = metaData.getColumns(null, null, tableName, null);
+
+            while (columns.next()) {
+                String columnName = columns.getString("COLUMN_NAME");
+                String dataType = columns.getString("TYPE_NAME");
+                int columnSize = columns.getInt("COLUMN_SIZE");
+                String isNullable = columns.getString("IS_NULLABLE");
+                boolean isAutoIncrement = columns.getBoolean("IS_AUTOINCREMENT");
+
+                System.out.println("Nome da Coluna: " + columnName);
+                System.out.println("Tipo de Dado: " + dataType);
+                System.out.println("Tamanho: " + columnSize);
+                System.out.println("É Nulo: " + isNullable);
+                System.out.println("Auto Incremento: " + isAutoIncrement);
+                System.out.println("---------------------");
+            }
+            
+            connection.close();
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
