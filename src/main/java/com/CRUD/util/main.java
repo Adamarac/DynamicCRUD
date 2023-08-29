bbca1dda44128562f8bb22b62588433d8d652252
Alan Moura <alan.moura007@gmail.com>
2023-08-28 21:59:45 -0300

    Creating the Testing Package
    Adding a Testing Package and Creating the Class That Retrieves Metadata from a Database Table

--- src/main/java/com/CRUD/util/main.java
+++ src/main/java/com/CRUD/util/main.java
@@ -1,8 +1,9 @@
 package com.CRUD.util;
 
+import com.CRUD.testes.tableMetadata;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
@@ -19,27 +20,11 @@ public class main {
                 System.out.println("Falha ao conectar ao banco de dados.");
             }
             
-            DatabaseMetaData metaData = connection.getMetaData();
             
             String ler = JOptionPane.showInputDialog("Digite a tabela que deseja consultar");
+            
+            tableMetadata.Metadata(ler, connection);
 
-            String tableName = ler;
-            ResultSet columns = metaData.getColumns(null, null, tableName, null);
-
-            while (columns.next()) {
-                String columnName = columns.getString("COLUMN_NAME");
-                String dataType = columns.getString("TYPE_NAME");
-                int columnSize = columns.getInt("COLUMN_SIZE");
-                String isNullable = columns.getString("IS_NULLABLE");
-                boolean isAutoIncrement = columns.getBoolean("IS_AUTOINCREMENT");
-
-                System.out.println("Nome da Coluna: " + columnName);
-                System.out.println("Tipo de Dado: " + dataType);
-                System.out.println("Tamanho: " + columnSize);
-                System.out.println("É Nulo: " + isNullable);
-                System.out.println("Auto Incremento: " + isAutoIncrement);
-                System.out.println("---------------------");
-            }
             
             connection.close();
             
